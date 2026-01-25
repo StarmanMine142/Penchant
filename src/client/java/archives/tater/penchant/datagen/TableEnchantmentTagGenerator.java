e3f7126ea1e4fbf3d6e200b1f8342499e642ee96
ThePotatoArchivist <josiahfu@gmail.com>
2026-01-25 02:03:00 -0800

    loot tweaks

--- src/client/java/archives/tater/penchant/datagen/TableEnchantmentTagGenerator.java
+++ src/client/java/archives/tater/penchant/datagen/TableEnchantmentTagGenerator.java
@@ -1,11 +1,14 @@
 package archives.tater.penchant.datagen;
 
+import archives.tater.penchant.Penchant;
+
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
 
 import net.minecraft.core.HolderLookup;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.tags.EnchantmentTags;
+import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.item.enchantment.Enchantments;
 
@@ -18,17 +21,31 @@ public class TableEnchantmentTagGenerator extends FabricTagProvider<Enchantment>
 
     @Override
     protected void addTags(HolderLookup.Provider wrapperLookup) {
+        var normalTreasure = TagKey.create(Registries.ENCHANTMENT, Penchant.id("normal_treasure"));
+
+        //noinspection unchecked
+        builder(normalTreasure).add(
+                Enchantments.FROST_WALKER,
+                Enchantments.FIRE_ASPECT,
+                Enchantments.FLAME,
+                Enchantments.SILK_TOUCH,
+                Enchantments.LUNGE,
+                Enchantments.CHANNELING,
+                Enchantments.RIPTIDE,
+                Enchantments.FORTUNE,
+                Enchantments.THORNS,
+                Enchantments.INFINITY,
+                Enchantments.RESPIRATION
+        );
         builder(EnchantmentTags.IN_ENCHANTING_TABLE)
-                .tagex_exclude(Enchantments.FIRE_ASPECT)
-                .tagex_exclude(Enchantments.FLAME)
-                .tagex_exclude(Enchantments.SILK_TOUCH)
-                .tagex_exclude(Enchantments.LUNGE)
-                .tagex_exclude(Enchantments.CHANNELING)
-                .tagex_exclude(Enchantments.RIPTIDE)
-                .tagex_exclude(Enchantments.FORTUNE)
-                .tagex_exclude(Enchantments.THORNS)
-                .tagex_exclude(Enchantments.INFINITY)
-                .tagex_exclude(Enchantments.RESPIRATION)
-                ;
+                .tagex_excludeTag(normalTreasure);
+        builder(EnchantmentTags.TRADEABLE)
+                .addTag(normalTreasure);
+        builder(EnchantmentTags.ON_RANDOM_LOOT)
+                .addTag(normalTreasure);
+        builder(EnchantmentTags.ON_TRADED_EQUIPMENT)
+                .addTag(normalTreasure);
+        builder(EnchantmentTags.ON_MOB_SPAWN_EQUIPMENT)
+                .addTag(normalTreasure);
     }
 }
